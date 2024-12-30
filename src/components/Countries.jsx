e8438b752f619c5992efa0305ccbd88ea2f1d8e5
Daniel Martinez <edu211004@gmail.com>
2024-12-30 12:52:57 -0600

    rendering mock

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,17 +1,46 @@
-import  { useState } from 'react'
+import { useState } from 'react'
+import api from '../mocks/api.json'
 
 function Countries() {
-  const [country, setCountry] = useState('')
-    const handleSubmit = ()=>{}
+    const [country, setCountry] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(country)
+    }
     return (
-    <div>
-        <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="countryInput">Country </label>
-            <input type="text" id='countryInput'/>
-            <button>Search</button>
-        </form>
-    </div>
-  )
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <label htmlFor="countryInput">Country </label>
+                <input type="text" id='countryInput' value={country} onChange={(e) => setCountry(e.target.value)} />
+                <button>Search</button>
+            </form>
+            <Country />
+        </div>
+    )
 }
 
+const Country = () => {
+    return (
+        <div style={{ width: '300px', overflow: 'hidden', padding: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+            <div style={{ width: '100%'}}>
+                <img
+                    src={api.flags.svg}
+                    alt=""
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover'
+                    }}
+                />
+            </div>
+            <h2>{api.name.common}</h2>
+            <p>{api.name.official}</p>
+            <ul>
+                <li>Population: {api.population}</li>
+                <li>Timezones: {api.timezones}</li>
+            </ul>
+        </div>
+
+    )
+}
 export default Countries
