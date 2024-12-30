35d0716922ef953cb48876b1d93d7ce2582d1f68
Daniel Martinez <edu211004@gmail.com>
2024-12-30 11:30:13 -0600

    custom counter hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const useCounter = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => setCounter(counter + 1)
+  const decrement = () => setCounter(counter - 1)
+  const setZero = () => setCounter(0)
+
+  return {
+    increment, decrement, setZero
+  }
+}
+
 function App() {
   return (
     <div>App</div>
